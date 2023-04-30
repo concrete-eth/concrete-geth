438d4246dff7d5995df0bb1733561a9eea2f319a
bytes <therealbyt3s@gmail.com>
2023-04-30 21:09:53 +0200

    test: add typical pc for testing

---
+++ tinygo/precompiles/typical/typical.go
@@ -0,0 +1,28 @@
+// Copyright 2023 The concrete-geth Authors
+//
+// The concrete-geth library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The concrete library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the concrete library. If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"github.com/ethereum/go-ethereum/concrete/lib"
+	"github.com/ethereum/go-ethereum/tinygo"
+)
+
+func init() {
+	tinygo.WasmWrap(&lib.TypicalPrecompile{}, false)
+}
+
+// main is REQUIRED for TinyGo to compile to Wasm
+func main() {}
