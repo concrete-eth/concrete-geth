73fda23adea37af2f0c9bb14d3936d250bb5e38a
bytes <therealbyt3s@gmail.com>
2023-05-31 21:36:29 +0200

    refactor: generalize pi registry pc

--- concrete/precompiles/preimage_registry.go
+++ concrete/precompiles/preimage_registry.go
@@ -15,18 +15,6 @@ import (
 //go:embed sol/abi/PreimageRegistry.abi
 var preimageRegistryABI string
 
-var (
-	AddPreimagePerByteGas = uint64(10)
-	HasPreimageGas        = uint64(10)
-	GetPreimageSizeGas    = uint64(10)
-	GetPreimagePerByteGas = uint64(10)
-
-	AddBigPreimagePerByteGas = uint64(10) // disabled
-	HasBigPreimageGas        = uint64(10)
-	GetBigPreimageSizeGas    = uint64(10)
-	GetBigPreimagePerByteGas = uint64(10)
-)
-
 func init() {
 	abiReader := strings.NewReader(preimageRegistryABI)
 	ABI, err := abi.JSON(abiReader)
@@ -34,12 +22,21 @@ func init() {
 		panic(err)
 	}
 
-	preimageRegistry := lib.NewPrecompileWithABI(ABI, map[string]lib.MethodPrecompile{
-		"addPreimage":     &addPreimage{},
-		"hasPreimage":     &hasPreimage{},
-		"getPreimageSize": &getPreimageSize{},
-		"getPreimage":     &getPreimage{},
-	})
+	preimageRegistry := NewPreimageRegistry(
+		ABI,
+		func(concrete api.API) api.PreimageStore {
+			return concrete.Persistent()
+		},
+		preimageRegistryGasTable{
+			addPreimageGas:        10,
+			addPreimagePerByteGas: 10,
+			hasPreimageGas:        10,
+			getPreimageSizeGas:    10,
+			getPreimageGas:        10,
+			getPreimagePerByteGas: 10,
+		},
+		false,
+	)
 	AddPrecompile(api.PreimageRegistryAddress, preimageRegistry, PrecompileMetadata{
 		Name:        "PreimageRegistry",
 		Version:     "0.1.0",
@@ -48,12 +45,21 @@ func init() {
 		Source:      "https://github.com/therealbytes/concrete-geth/tree/concrete/concrete/precompiles/pi_registry.go",
 	})
 
-	bigPreimageRegistry := lib.NewPrecompileWithABI(ABI, map[string]lib.MethodPrecompile{
-		"addPreimage":     &addBigPreimage{},
-		"hasPreimage":     &hasBigPreimage{},
-		"getPreimageSize": &getBigPreimageSize{},
-		"getPreimage":     &getBigPreimage{},
-	})
+	bigPreimageRegistry := NewPreimageRegistry(
+		ABI,
+		func(concrete api.API) api.PreimageStore {
+			return api.NewPersistentBigPreimageStore(concrete, -1, -1)
+		},
+		preimageRegistryGasTable{
+			addPreimageGas:        10,
+			addPreimagePerByteGas: 10,
+			hasPreimageGas:        10,
+			getPreimageSizeGas:    10,
+			getPreimageGas:        10,
+			getPreimagePerByteGas: 10,
+		},
+		false,
+	)
 	AddPrecompile(api.BigPreimageRegistryAddress, bigPreimageRegistry, PrecompileMetadata{
 		Name:        "BigPreimageRegistry",
 		Version:     "0.1.0",
@@ -63,56 +69,97 @@ func init() {
 	})
 }
 
+type storeGetter func(api.API) api.PreimageStore
+
+type preimageRegistryGasTable struct {
+	addPreimageGas        uint64
+	addPreimagePerByteGas uint64
+	hasPreimageGas        uint64
+	getPreimageSizeGas    uint64
+	getPreimageGas        uint64
+	getPreimagePerByteGas uint64
+}
+
+func NewPreimageRegistry(ABI abi.ABI, getStore storeGetter, gasTable preimageRegistryGasTable, enableWrites bool) api.Precompile {
+	return lib.NewPrecompileWithABI(ABI, map[string]lib.MethodPrecompile{
+		"addPreimage": &addPreimage{
+			getStore: getStore,
+			gasTable: &gasTable,
+			enabled:  enableWrites,
+		},
+		"hasPreimage": &hasPreimage{
+			getStore: getStore,
+			gasTable: &gasTable,
+		},
+		"getPreimageSize": &getPreimageSize{
+			getStore: getStore,
+			gasTable: &gasTable,
+		},
+		"getPreimage": &getPreimage{
+			getStore: getStore,
+			gasTable: &gasTable,
+		},
+	})
+}
+
 type addPreimage struct {
 	lib.BlankMethodPrecompile
+	getStore storeGetter
+	gasTable *preimageRegistryGasTable
+	enabled  bool
 }
 
 func (p *addPreimage) RequiredGas(input []byte) uint64 {
 	if len(input) < 64 {
 		return 0
 	}
-	return AddPreimagePerByteGas * uint64(len(input)-64)
+	return p.gasTable.addPreimageGas + p.gasTable.addPreimagePerByteGas*uint64(len(input)-64)
 }
 
 func (p *addPreimage) Run(concrete api.API, input []byte) ([]byte, error) {
+	if !p.enabled {
+		return nil, errors.New("writes to registry are disabled")
+	}
 	return p.CallRunWithArgs(func(concrete api.API, args []interface{}) ([]interface{}, error) {
 		preimage := args[0].([]byte)
-		hash := concrete.Persistent().AddPreimage(preimage)
+		hash := p.getStore(concrete).AddPreimage(preimage)
 		return []interface{}{hash}, nil
 	}, concrete, input)
 }
 
 type hasPreimage struct {
 	lib.BlankMethodPrecompile
+	getStore storeGetter
+	gasTable *preimageRegistryGasTable
 }
 
 func (p *hasPreimage) RequiredGas(input []byte) uint64 {
-	return HasPreimageGas
+	return p.gasTable.hasPreimageGas
 }
 
 func (p *hasPreimage) Run(concrete api.API, input []byte) ([]byte, error) {
 	return p.CallRunWithArgs(func(concrete api.API, args []interface{}) ([]interface{}, error) {
 		hash := common.Hash(args[0].([32]byte))
-		has := concrete.Persistent().HasPreimage(hash)
+		has := p.getStore(concrete).HasPreimage(hash)
 		return []interface{}{has}, nil
 	}, concrete, input)
 }
 
 type getPreimageSize struct {
 	lib.BlankMethodPrecompile
+	getStore storeGetter
+	gasTable *preimageRegistryGasTable
 }
 
 func (p *getPreimageSize) RequiredGas(input []byte) uint64 {
-	return GetPreimageSizeGas
+	return p.gasTable.getPreimageSizeGas
 }
 
 func (p *getPreimageSize) Run(concrete api.API, input []byte) ([]byte, error) {
 	return p.CallRunWithArgs(func(concrete api.API, args []interface{}) ([]interface{}, error) {
 		hash := common.Hash(args[0].([32]byte))
-		var size int
-		if concrete.Persistent().HasPreimage(hash) {
-			size = concrete.StateDB().GetPersistentPreimageSize(hash)
-		} else {
+		size := p.getStore(concrete).GetPreimageSize(hash)
+		if size < 0 {
 			size = 0
 		}
 		return []interface{}{big.NewInt(int64(size))}, nil
@@ -121,12 +168,14 @@ func (p *getPreimageSize) Run(concrete api.API, input []byte) ([]byte, error) {
 
 type getPreimage struct {
 	lib.BlankMethodPrecompile
+	getStore storeGetter
+	gasTable *preimageRegistryGasTable
 }
 
 func (p *getPreimage) RequiredGas(input []byte) uint64 {
 	return p.CallRequiredGasWithArgs(func(args []interface{}) uint64 {
 		size := args[0].(*big.Int)
-		return GetPreimagePerByteGas * uint64(size.Int64())
+		return p.gasTable.getPreimageGas + p.gasTable.getPreimagePerByteGas*uint64(size.Int64())
 	}, input)
 }
 
@@ -134,87 +183,7 @@ func (p *getPreimage) Run(concrete api.API, input []byte) ([]byte, error) {
 	return p.CallRunWithArgs(func(concrete api.API, args []interface{}) ([]interface{}, error) {
 		size := int(args[0].(*big.Int).Int64())
 		hash := common.Hash(args[1].([32]byte))
-		per := concrete.Persistent()
-		if !per.HasPreimage(hash) {
-			return []interface{}{[]byte{}}, errors.New("preimage not found")
-		}
-		realSize := per.GetPreimageSize(hash)
-		if size < realSize {
-			return []interface{}{[]byte{}}, errors.New("provided preimage size too small")
-		}
-		preimage := per.GetPreimage(hash)
-		return []interface{}{preimage}, nil
-	}, concrete, input)
-}
-
-type addBigPreimage struct {
-	lib.BlankMethodPrecompile
-}
-
-func (p *addBigPreimage) RequiredGas(input []byte) uint64 {
-	if len(input) < 64 {
-		return 0
-	}
-	return AddBigPreimagePerByteGas * uint64(len(input)-64)
-}
-
-func (p *addBigPreimage) Run(concrete api.API, input []byte) ([]byte, error) {
-	return p.CallRunWithArgs(func(concrete api.API, args []interface{}) ([]interface{}, error) {
-		hash := common.Hash{}
-		return []interface{}{hash}, errors.New("not implemented")
-	}, concrete, input)
-}
-
-type hasBigPreimage struct {
-	lib.BlankMethodPrecompile
-}
-
-func (p *hasBigPreimage) RequiredGas(input []byte) uint64 {
-	return HasBigPreimageGas
-}
-
-func (p *hasBigPreimage) Run(concrete api.API, input []byte) ([]byte, error) {
-	return p.CallRunWithArgs(func(concrete api.API, args []interface{}) ([]interface{}, error) {
-		hash := common.Hash(args[0].([32]byte))
-		store := api.NewPersistentBigPreimageStore(concrete, -1, -1)
-		has := store.HasPreimage(hash)
-		return []interface{}{has}, nil
-	}, concrete, input)
-}
-
-type getBigPreimageSize struct {
-	lib.BlankMethodPrecompile
-}
-
-func (p *getBigPreimageSize) RequiredGas(input []byte) uint64 {
-	return GetBigPreimageSizeGas
-}
-
-func (p *getBigPreimageSize) Run(concrete api.API, input []byte) ([]byte, error) {
-	return p.CallRunWithArgs(func(concrete api.API, args []interface{}) ([]interface{}, error) {
-		hash := common.Hash(args[0].([32]byte))
-		store := api.NewPersistentBigPreimageStore(concrete, -1, -1)
-		size := store.GetPreimageSize(hash)
-		return []interface{}{big.NewInt(int64(size))}, nil
-	}, concrete, input)
-}
-
-type getBigPreimage struct {
-	lib.BlankMethodPrecompile
-}
-
-func (p *getBigPreimage) RequiredGas(input []byte) uint64 {
-	return p.CallRequiredGasWithArgs(func(args []interface{}) uint64 {
-		size := args[0].(*big.Int)
-		return GetBigPreimagePerByteGas * uint64(size.Int64())
-	}, input)
-}
-
-func (p *getBigPreimage) Run(concrete api.API, input []byte) ([]byte, error) {
-	return p.CallRunWithArgs(func(concrete api.API, args []interface{}) ([]interface{}, error) {
-		size := int(args[0].(*big.Int).Int64())
-		hash := common.Hash(args[1].([32]byte))
-		store := api.NewPersistentBigPreimageStore(concrete, -1, -1)
+		store := p.getStore(concrete)
 		if !store.HasPreimage(hash) {
 			return []interface{}{[]byte{}}, errors.New("preimage not found")
 		}
