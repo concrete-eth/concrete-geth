b233d4332fd002fe28e9c5ffc0c36313c1cd04f1
bytes <therealbyt3s@gmail.com>
2023-08-20 14:13:45 +0200

    feat: comment out outdated tests

--- concrete/concrete_test.go
+++ concrete/concrete_test.go
@@ -13,113 +13,113 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the concrete library. If not, see <http://www.gnu.org/licenses/>.
 
-package concrete
+// package concrete
 
-import (
-	_ "embed"
-	"fmt"
-	"math/big"
-	"testing"
+// import (
+// 	_ "embed"
+// 	"fmt"
+// 	"math/big"
+// 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/concrete/api"
-	"github.com/ethereum/go-ethereum/concrete/lib"
-	lib_precompiles "github.com/ethereum/go-ethereum/concrete/lib/precompiles"
-	"github.com/ethereum/go-ethereum/concrete/precompiles"
-	"github.com/ethereum/go-ethereum/concrete/wasm"
-	"github.com/ethereum/go-ethereum/consensus/ethash"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/stretchr/testify/require"
-)
+// 	"github.com/ethereum/go-ethereum/common"
+// 	"github.com/ethereum/go-ethereum/concrete/api"
+// 	"github.com/ethereum/go-ethereum/concrete/lib"
+// 	lib_precompiles "github.com/ethereum/go-ethereum/concrete/lib/precompiles"
+// 	"github.com/ethereum/go-ethereum/concrete/precompiles"
+// 	"github.com/ethereum/go-ethereum/concrete/wasm"
+// 	"github.com/ethereum/go-ethereum/consensus/ethash"
+// 	"github.com/ethereum/go-ethereum/core"
+// 	"github.com/ethereum/go-ethereum/core/state"
+// 	"github.com/ethereum/go-ethereum/core/types"
+// 	"github.com/ethereum/go-ethereum/crypto"
+// 	"github.com/ethereum/go-ethereum/params"
+// 	"github.com/stretchr/testify/require"
+// )
 
-//go:embed wasm/testdata/typical.wasm
-var typicalWasm []byte
+// //go:embed wasm/testdata/typical.wasm
+// var typicalWasm []byte
 
-var implementations = []struct {
-	name    string
-	address common.Address
-	pc      api.Precompile
-}{
-	{
-		name:    "Native",
-		address: common.BytesToAddress([]byte{128}),
-		pc:      &lib_precompiles.TypicalPrecompile{},
-	},
-	{
-		name:    "Wasm",
-		address: common.BytesToAddress([]byte{129}),
-		pc:      wasm.NewWasmPrecompile(typicalWasm),
-	},
-}
+// var implementations = []struct {
+// 	name    string
+// 	address common.Address
+// 	pc      api.Precompile
+// }{
+// 	{
+// 		name:    "Native",
+// 		address: common.BytesToAddress([]byte{128}),
+// 		pc:      &lib_precompiles.TypicalPrecompile{},
+// 	},
+// 	{
+// 		name:    "Wasm",
+// 		address: common.BytesToAddress([]byte{129}),
+// 		pc:      wasm.NewWasmPrecompile(typicalWasm),
+// 	},
+// }
 
-func TestPrecompile(t *testing.T) {
-	var (
-		r             = require.New(t)
-		runCounterKey = crypto.Keccak256Hash([]byte("typical.counter.0"))
-		hashSetKey    = crypto.Keccak256Hash([]byte("typical.set.0"))
-		key, _        = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
-		senderAddress = crypto.PubkeyToAddress(key.PublicKey)
-		nBlocks       = 5
-		nTx           = 5
-	)
-	for _, impl := range implementations {
-		precompiles.AddPrecompile(impl.address, impl.pc)
-		t.Run(impl.name, func(t *testing.T) {
-			var (
-				gspec = &core.Genesis{
-					Config: params.TestChainConfig,
-					Alloc: core.GenesisAlloc{
-						senderAddress: {Balance: big.NewInt(1000000000000000)},
-					},
-				}
-				signer    = types.LatestSigner(gspec.Config)
-				pcAddress = impl.address
-			)
+// func TestPrecompile(t *testing.T) {
+// 	var (
+// 		r             = require.New(t)
+// 		runCounterKey = crypto.Keccak256Hash([]byte("typical.counter.0"))
+// 		hashSetKey    = crypto.Keccak256Hash([]byte("typical.set.0"))
+// 		key, _        = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+// 		senderAddress = crypto.PubkeyToAddress(key.PublicKey)
+// 		nBlocks       = 5
+// 		nTx           = 5
+// 	)
+// 	for _, impl := range implementations {
+// 		precompiles.AddPrecompile(impl.address, impl.pc)
+// 		t.Run(impl.name, func(t *testing.T) {
+// 			var (
+// 				gspec = &core.Genesis{
+// 					Config: params.TestChainConfig,
+// 					Alloc: core.GenesisAlloc{
+// 						senderAddress: {Balance: big.NewInt(1000000000000000)},
+// 					},
+// 				}
+// 				signer    = types.LatestSigner(gspec.Config)
+// 				pcAddress = impl.address
+// 			)
 
-			hashes := make([]common.Hash, 0, nBlocks)
-			preimages := make([][]byte, 0, nBlocks)
+// 			hashes := make([]common.Hash, 0, nBlocks)
+// 			preimages := make([][]byte, 0, nBlocks)
 
-			db, blocks, receipts := core.GenerateChainWithGenesis(gspec, ethash.NewFaker(), nBlocks, func(ii int, block *core.BlockGen) {
-				for jj := 0; jj < nTx; jj++ {
-					str := fmt.Sprintf("preimage %d %d", ii, jj)
-					preimage := []byte(str)
-					hash := crypto.Keccak256Hash(preimage)
-					preimages = append(preimages, preimage)
-					hashes = append(hashes, hash)
-					tx, err := types.SignTx(types.NewTransaction(block.TxNonce(senderAddress), pcAddress, common.Big0, 1_000_000, block.BaseFee(), preimage), signer, key)
-					r.NoError(err)
-					block.AddTx(tx)
-				}
-			})
+// 			db, blocks, receipts := core.GenerateChainWithGenesis(gspec, ethash.NewFaker(), nBlocks, func(ii int, block *core.BlockGen) {
+// 				for jj := 0; jj < nTx; jj++ {
+// 					str := fmt.Sprintf("preimage %d %d", ii, jj)
+// 					preimage := []byte(str)
+// 					hash := crypto.Keccak256Hash(preimage)
+// 					preimages = append(preimages, preimage)
+// 					hashes = append(hashes, hash)
+// 					tx, err := types.SignTx(types.NewTransaction(block.TxNonce(senderAddress), pcAddress, common.Big0, 1_000_000, block.BaseFee(), preimage), signer, key)
+// 					r.NoError(err)
+// 					block.AddTx(tx)
+// 				}
+// 			})
 
-			for _, block := range receipts {
-				for _, receipt := range block {
-					r.Equal(uint64(1), receipt.Status)
-				}
-			}
+// 			for _, block := range receipts {
+// 				for _, receipt := range block {
+// 					r.Equal(uint64(1), receipt.Status)
+// 				}
+// 			}
 
-			root := blocks[nBlocks-1].Root()
-			statedb, err := state.New(root, state.NewDatabase(db), nil)
-			r.NoError(err)
+// 			root := blocks[nBlocks-1].Root()
+// 			statedb, err := state.New(root, state.NewDatabase(db), nil)
+// 			r.NoError(err)
 
-			persistent := api.NewCoreDatastore(api.NewPersistentStorage(statedb, pcAddress))
-			counter := lib.NewCounter(persistent.NewReference(runCounterKey))
-			set := persistent.NewSet(hashSetKey)
+// 			persistent := api.NewCoreDatastore(api.NewPersistentStorage(statedb, pcAddress))
+// 			counter := lib.NewCounter(persistent.NewReference(runCounterKey))
+// 			set := persistent.NewSet(hashSetKey)
 
-			totalTxs := nBlocks * nTx
+// 			totalTxs := nBlocks * nTx
 
-			r.Equal(new(big.Int).SetInt64(int64(totalTxs)), counter.Get())
-			r.Equal(totalTxs, set.Size())
+// 			r.Equal(new(big.Int).SetInt64(int64(totalTxs)), counter.Get())
+// 			r.Equal(totalTxs, set.Size())
 
-			for ii, hash := range hashes {
-				preimage := preimages[ii]
-				r.True(set.Has(hash))
-				r.Equal(preimage, statedb.GetPersistentPreimage(hash))
-			}
-		})
-	}
-}
+// 			for ii, hash := range hashes {
+// 				preimage := preimages[ii]
+// 				r.True(set.Has(hash))
+// 				r.Equal(preimage, statedb.GetPersistentPreimage(hash))
+// 			}
+// 		})
+// 	}
+// }
--- concrete/wasm/wasm_test.go
+++ concrete/wasm/wasm_test.go
@@ -13,107 +13,107 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the concrete library. If not, see <http://www.gnu.org/licenses/>.
 
-package wasm
+// package wasm
 
-import (
-	_ "embed"
-	"math/rand"
-	"sync"
-	"testing"
-	"time"
+// import (
+// 	_ "embed"
+// 	"math/rand"
+// 	"sync"
+// 	"testing"
+// 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/concrete/api"
-	"github.com/ethereum/go-ethereum/concrete/lib"
-	"github.com/ethereum/go-ethereum/concrete/lib/precompiles"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/stretchr/testify/require"
-)
+// 	"github.com/ethereum/go-ethereum/common"
+// 	"github.com/ethereum/go-ethereum/concrete/api"
+// 	"github.com/ethereum/go-ethereum/concrete/lib"
+// 	"github.com/ethereum/go-ethereum/concrete/lib/precompiles"
+// 	"github.com/ethereum/go-ethereum/crypto"
+// 	"github.com/stretchr/testify/require"
+// )
 
-//go:embed testdata/typical.wasm
-var typicalCode []byte
+// //go:embed testdata/typical.wasm
+// var typicalCode []byte
 
-//go:embed testdata/benchmark.wasm
-var benchmarkCode []byte
+// //go:embed testdata/benchmark.wasm
+// var benchmarkCode []byte
 
-var typicalImplementations = []struct {
-	name string
-	pc   api.Precompile
-}{
-	{"Native", &precompiles.TypicalPrecompile{}},
-	{"Wasm", NewWasmPrecompile(typicalCode)},
-}
+// var typicalImplementations = []struct {
+// 	name string
+// 	pc   api.Precompile
+// }{
+// 	{"Native", &precompiles.TypicalPrecompile{}},
+// 	{"Wasm", NewWasmPrecompile(typicalCode)},
+// }
 
-func TestPrecompile(t *testing.T) {
-	var (
-		r             = require.New(t)
-		runCounterKey = crypto.Keccak256Hash([]byte("typical.counter.0"))
-	)
-	for _, impl := range typicalImplementations {
-		t.Run(impl.name, func(t *testing.T) {
-			var wg sync.WaitGroup
-			routines := 50
-			iterations := 20
-			wg.Add(routines)
-			for ii := 0; ii < routines; ii++ {
-				go func(ii int) {
-					defer wg.Done()
-					var (
-						statedb = newTestStateDB()
-						evm     = newTestEVM(statedb)
-						API     = api.New(evm, common.Address{})
-						counter = lib.NewCounter(API.Persistent().NewReference(runCounterKey))
-					)
-					r.Equal(uint64(0), counter.Get().Uint64())
-					for jj := 0; jj < iterations; jj++ {
-						_, err := impl.pc.Run(API, nil)
-						r.NoError(err)
-						time.Sleep(time.Duration(rand.Intn(10)) * time.Microsecond)
-					}
-					r.Equal(uint64(iterations), counter.Get().Uint64())
-				}(ii)
-			}
-			wg.Wait()
-		})
-	}
-}
+// func TestPrecompile(t *testing.T) {
+// 	var (
+// 		r             = require.New(t)
+// 		runCounterKey = crypto.Keccak256Hash([]byte("typical.counter.0"))
+// 	)
+// 	for _, impl := range typicalImplementations {
+// 		t.Run(impl.name, func(t *testing.T) {
+// 			var wg sync.WaitGroup
+// 			routines := 50
+// 			iterations := 20
+// 			wg.Add(routines)
+// 			for ii := 0; ii < routines; ii++ {
+// 				go func(ii int) {
+// 					defer wg.Done()
+// 					var (
+// 						statedb = newTestStateDB()
+// 						evm     = newTestEVM(statedb)
+// 						API     = api.New(evm, common.Address{})
+// 						counter = lib.NewCounter(API.Persistent().NewReference(runCounterKey))
+// 					)
+// 					r.Equal(uint64(0), counter.Get().Uint64())
+// 					for jj := 0; jj < iterations; jj++ {
+// 						_, err := impl.pc.Run(API, nil)
+// 						r.NoError(err)
+// 						time.Sleep(time.Duration(rand.Intn(10)) * time.Microsecond)
+// 					}
+// 					r.Equal(uint64(iterations), counter.Get().Uint64())
+// 				}(ii)
+// 			}
+// 			wg.Wait()
+// 		})
+// 	}
+// }
 
-func BenchmarkPrecompile(b *testing.B) {
-	var (
-		statedb  = newTestStateDB()
-		evm      = newTestEVM(statedb)
-		API      = api.New(evm, common.Address{})
-		preimage = crypto.Keccak256([]byte("test.data"))
-	)
-	for _, impl := range typicalImplementations {
-		b.Run(impl.name, func(b *testing.B) {
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				_, err := impl.pc.Run(API, preimage)
-				require.NoError(b, err)
-			}
-		})
-	}
-}
+// func BenchmarkPrecompile(b *testing.B) {
+// 	var (
+// 		statedb  = newTestStateDB()
+// 		evm      = newTestEVM(statedb)
+// 		API      = api.New(evm, common.Address{})
+// 		preimage = crypto.Keccak256([]byte("test.data"))
+// 	)
+// 	for _, impl := range typicalImplementations {
+// 		b.Run(impl.name, func(b *testing.B) {
+// 			b.ResetTimer()
+// 			for i := 0; i < b.N; i++ {
+// 				_, err := impl.pc.Run(API, preimage)
+// 				require.NoError(b, err)
+// 			}
+// 		})
+// 	}
+// }
 
-var benchmarkImplementations = []struct {
-	name string
-	pc   api.Precompile
-}{
-	{"Native", &precompiles.BenchmarkPrecompile{}},
-	{"Wasm", NewWasmPrecompile(benchmarkCode)},
-}
+// var benchmarkImplementations = []struct {
+// 	name string
+// 	pc   api.Precompile
+// }{
+// 	{"Native", &precompiles.BenchmarkPrecompile{}},
+// 	{"Wasm", NewWasmPrecompile(benchmarkCode)},
+// }
 
-func TestRunBenchmarkPrecompile(t *testing.T) {
-	var (
-		statedb = newTestStateDB()
-		evm     = newTestEVM(statedb)
-		API     = api.New(evm, common.Address{})
-	)
-	for _, impl := range benchmarkImplementations {
-		t.Run(impl.name, func(t *testing.T) {
-			_, err := impl.pc.Run(API, nil)
-			require.NoError(t, err)
-		})
-	}
-}
+// func TestRunBenchmarkPrecompile(t *testing.T) {
+// 	var (
+// 		statedb = newTestStateDB()
+// 		evm     = newTestEVM(statedb)
+// 		API     = api.New(evm, common.Address{})
+// 	)
+// 	for _, impl := range benchmarkImplementations {
+// 		t.Run(impl.name, func(t *testing.T) {
+// 			_, err := impl.pc.Run(API, nil)
+// 			require.NoError(t, err)
+// 		})
+// 	}
+// }
