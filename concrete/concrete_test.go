ad5c7b297f875bbbd950d0930839c0808caf8986
bytes <therealbyt3s@gmail.com>
2023-08-23 21:20:14 +0200

    test: e2e kkv test

--- concrete/concrete_test.go
+++ concrete/concrete_test.go
@@ -25,9 +25,16 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/concrete/api"
 	"github.com/ethereum/go-ethereum/concrete/fixtures"
+	"github.com/ethereum/go-ethereum/concrete/lib"
 	"github.com/ethereum/go-ethereum/concrete/mock"
 	"github.com/ethereum/go-ethereum/concrete/precompiles"
 	"github.com/ethereum/go-ethereum/concrete/wasm"
+	"github.com/ethereum/go-ethereum/consensus/ethash"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/params"
 )
 
 //go:embed fixtures/build/add.wasm
@@ -180,70 +187,75 @@ func TestKkvPrecompileFixture(t *testing.T) {
 	}
 }
 
-// func TestPrecompile(t *testing.T) {
-// 	var (
-// 		r             = require.New(t)
-// 		runCounterKey = crypto.Keccak256Hash([]byte("typical.counter.0"))
-// 		hashSetKey    = crypto.Keccak256Hash([]byte("typical.set.0"))
-// 		key, _        = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
-// 		senderAddress = crypto.PubkeyToAddress(key.PublicKey)
-// 		nBlocks       = 5
-// 		nTx           = 5
-// 	)
-// 	for _, impl := range implementations {
-// 		precompiles.AddPrecompile(impl.address, impl.pc)
-// 		t.Run(impl.name, func(t *testing.T) {
-// 			var (
-// 				gspec = &core.Genesis{
-// 					Config: params.TestChainConfig,
-// 					Alloc: core.GenesisAlloc{
-// 						senderAddress: {Balance: big.NewInt(1000000000000000)},
-// 					},
-// 				}
-// 				signer    = types.LatestSigner(gspec.Config)
-// 				pcAddress = impl.address
-// 			)
+var kkvImplementationsE2E = []struct {
+	name    string
+	address common.Address
+	pc      precompiles.Precompile
+}{
+	{
+		name:    "Native",
+		address: common.BytesToAddress([]byte{132}),
+		pc:      &fixtures.KeyKeyValuePrecompile{},
+	},
+	// {
+	// 	name:    "Wasm",
+	// 	address: common.BytesToAddress([]byte{133}),
+	// 	pc:      wasm.NewWasmPrecompile(kkvWasm),
+	// },
+}
 
-// 			hashes := make([]common.Hash, 0, nBlocks)
-// 			preimages := make([][]byte, 0, nBlocks)
-
-// 			db, blocks, receipts := core.GenerateChainWithGenesis(gspec, ethash.NewFaker(), nBlocks, func(ii int, block *core.BlockGen) {
-// 				for jj := 0; jj < nTx; jj++ {
-// 					str := fmt.Sprintf("preimage %d %d", ii, jj)
-// 					preimage := []byte(str)
-// 					hash := crypto.Keccak256Hash(preimage)
-// 					preimages = append(preimages, preimage)
-// 					hashes = append(hashes, hash)
-// 					tx, err := types.SignTx(types.NewTransaction(block.TxNonce(senderAddress), pcAddress, common.Big0, 1_000_000, block.BaseFee(), preimage), signer, key)
-// 					r.NoError(err)
-// 					block.AddTx(tx)
-// 				}
-// 			})
-
-// 			for _, block := range receipts {
-// 				for _, receipt := range block {
-// 					r.Equal(uint64(1), receipt.Status)
-// 				}
-// 			}
-
-// 			root := blocks[nBlocks-1].Root()
-// 			statedb, err := state.New(root, state.NewDatabase(db), nil)
-// 			r.NoError(err)
-
-// 			persistent := api.NewCoreDatastore(api.NewPersistentStorage(statedb, pcAddress))
-// 			counter := lib.NewCounter(persistent.NewReference(runCounterKey))
-// 			set := persistent.NewSet(hashSetKey)
-
-// 			totalTxs := nBlocks * nTx
-
-// 			r.Equal(new(big.Int).SetInt64(int64(totalTxs)), counter.Get())
-// 			r.Equal(totalTxs, set.Size())
-
-// 			for ii, hash := range hashes {
-// 				preimage := preimages[ii]
-// 				r.True(set.Has(hash))
-// 				r.Equal(preimage, statedb.GetPersistentPreimage(hash))
-// 			}
-// 		})
-// 	}
-// }
+func TestE2EKkvPrecompile(t *testing.T) {
+	var (
+		ABI           = getKkvABI()
+		key, _        = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+		senderAddress = crypto.PubkeyToAddress(key.PublicKey)
+		nBlocks       = 3
+	)
+	for _, impl := range kkvImplementationsE2E {
+		precompiles.AddPrecompile(impl.address, impl.pc)
+	}
+	for _, impl := range kkvImplementationsE2E {
+		t.Run(impl.name, func(t *testing.T) {
+			var (
+				gspec = &core.Genesis{
+					Config: params.TestChainConfig,
+					Alloc: core.GenesisAlloc{
+						senderAddress: {Balance: big.NewInt(1e15)},
+					},
+				}
+				signer = types.LatestSigner(gspec.Config)
+			)
+			db, blocks, _ := core.GenerateChainWithGenesis(gspec, ethash.NewFaker(), nBlocks, func(ii int, block *core.BlockGen) {
+				k1 := common.BigToHash(big.NewInt(int64(ii)))
+				k2 := common.BigToHash(big.NewInt(int64(ii + 1)))
+				v := common.BigToHash(big.NewInt(int64(ii + 2)))
+				input, err := ABI.Pack("set", k1, k2, v)
+				if err != nil {
+					t.Fatal(err)
+				}
+				tx := types.NewTransaction(block.TxNonce(senderAddress), impl.address, common.Big0, 1e5, block.BaseFee(), input)
+				signed, err := types.SignTx(tx, signer, key)
+				if err != nil {
+					t.Fatal(err)
+				}
+				block.AddTx(signed)
+			})
+			root := blocks[len(blocks)-1].Root()
+			statedb, err := state.New(root, state.NewDatabase(db), nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			env := api.NewNoCallEnvironment(impl.address, api.EnvConfig{}, statedb, false, 0)
+			kkv := lib.NewDatastore(env).Mapping([]byte("map.kkv.v1"))
+			for ii := 0; ii < nBlocks; ii++ {
+				k1 := common.BigToHash(big.NewInt(int64(ii)))
+				k2 := common.BigToHash(big.NewInt(int64(ii + 1)))
+				v := common.BigToHash(big.NewInt(int64(ii + 2)))
+				value := kkv.Mapping(k1.Bytes()).Value(k2.Bytes()).GetBytes32()
+				if value != v {
+					t.Fatalf("expected %s, got %s", v, value)
+				}
+			}
+		})
+	}
+}
