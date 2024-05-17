6f416bab5350aacf4dfe3c07e9580f02d873f27a
bytes <therealbyt3s@gmail.com>
2024-05-17 14:00:44 +0200

    refactor: edit block number accessor

--- miner/worker.go
+++ miner/worker.go
@@ -838,7 +838,7 @@ func (w *worker) applyTransaction(env *environment, tx *types.Transaction) (*typ
 		snap = env.state.Snapshot()
 		gp   = env.gasPool.Gas()
 	)
-	concretePcs := w.chain.Concrete().Precompiles(w.chain.CurrentBlock().Number.Uint64())
+	concretePcs := w.chain.Concrete().Precompiles(env.header.Number.Uint64())
 	receipt, err := core.ApplyTransaction(w.chainConfig, w.chain, &env.coinbase, env.gasPool, env.state, env.header, tx, &env.header.GasUsed, *w.chain.GetVMConfig(), concretePcs)
 	if err != nil {
 		env.state.RevertToSnapshot(snap)
