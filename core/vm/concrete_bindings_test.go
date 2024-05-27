5309e4cd1e2aa651b51058acc5050c006e05d8f1
Fabricio Robles <fabrobles92@gmail.com>
2024-05-27 17:16:31 -0600

    -Apply code review comments from bytes -Unit test for CallStatic -Unit test for Call -Unit test for Create -Unit test for Create2

--- core/vm/concrete_bindings_test.go
+++ core/vm/concrete_bindings_test.go
@@ -5,105 +5,201 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/params"
 	"github.com/holiman/uint256"
 	"github.com/stretchr/testify/require"
 )
 
-// Common ConcreteBlockContext object used by some tests
-func newConcreteBlockContext() *concreteBlockContext {
-	hash := crypto.Keccak256Hash([]byte("test coin base"))
-	coinbase := common.BytesToAddress(hash[:])
-	randomHash := crypto.Keccak256Hash([]byte("random hash"))
+func newBlockContext() BlockContext {
+	var (
+		block0Hash = crypto.Keccak256Hash([]byte("test0"))
+		block1Hash = crypto.Keccak256Hash([]byte("test1"))
+		randomHash = crypto.Keccak256Hash([]byte("random hash"))
+	)
+
+	getHash := func(blockNumber uint64) common.Hash {
+		switch blockNumber {
+		case 0:
+			return block0Hash
+		case 1:
+			return block1Hash
+		}
+		return common.Hash{}
+	}
 
 	vmctx := BlockContext{
+		CanTransfer: func(StateDB, common.Address, *uint256.Int) bool { return true },
+		Transfer:    func(StateDB, common.Address, common.Address, *uint256.Int) {},
 		BlockNumber: big.NewInt(0),
-		GetHash:     func(uint64) common.Hash { return crypto.Keccak256Hash([]byte("test")) },
+		GetHash:     getHash,
 		Time:        50,
 		GasLimit:    1000,
 		Difficulty:  big.NewInt(23456146),
 		BaseFee:     big.NewInt(739657255),
-		Coinbase:    coinbase,
+		Coinbase:    common.HexToAddress("0x0854167430392BBc2D15Dd1Cc17e761897AF31C9"),
 		Random:      &randomHash,
 	}
-	return &concreteBlockContext{ctx: &vmctx}
+
+	return vmctx
 }
 
-func TestConcreteGetHash(t *testing.T) {
+func TestConcreteBlockContext(t *testing.T) {
 	r := require.New(t)
-	concreteBlockContext := *newConcreteBlockContext()
-	expectedHash := common.HexToHash("0x9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658")
+	blockCtx := newBlockContext()
+	ccBlockCtx := concreteBlockContext{ctx: &blockCtx}
+	var (
+		block0Hash = crypto.Keccak256Hash([]byte("test0"))
+		block1Hash = crypto.Keccak256Hash([]byte("test1"))
+	)
 
-	hash := concreteBlockContext.GetHash(0)
+	t.Run("GetHash", func(t *testing.T) {
+		r.Equal(block0Hash, ccBlockCtx.GetHash(0))
+		r.Equal(block1Hash, ccBlockCtx.GetHash(1))
+	})
 
-	r.Equal(hash, expectedHash)
+	t.Run("Timestamp", func(t *testing.T) {
+		r.Equal(blockCtx.Time, ccBlockCtx.Timestamp())
+	})
+
+	t.Run("BlockNumber", func(t *testing.T) {
+		r.Equal(blockCtx.BlockNumber.Uint64(), ccBlockCtx.BlockNumber())
+	})
+
+	t.Run("GasLimit", func(t *testing.T) {
+		r.Equal(blockCtx.GasLimit, ccBlockCtx.GasLimit())
+	})
+
+	t.Run("Difficulty", func(t *testing.T) {
+		r.Equal(uint256.MustFromBig(blockCtx.Difficulty), ccBlockCtx.Difficulty())
+	})
+
+	t.Run("BaseFee", func(t *testing.T) {
+		r.Equal(uint256.MustFromBig(blockCtx.BaseFee), ccBlockCtx.BaseFee())
+	})
+
+	t.Run("Coinbase", func(t *testing.T) {
+		r.Equal(blockCtx.Coinbase, ccBlockCtx.Coinbase())
+	})
+
+	t.Run("Random", func(t *testing.T) {
+		r.Equal(*blockCtx.Random, ccBlockCtx.Random())
+	})
 }
 
-func TestConcreteTimeStamp(t *testing.T) {
-	r := require.New(t)
-	concreteBlockContext := *newConcreteBlockContext()
-	expectedTime := uint64(50)
+func TestEVMCallStatic(t *testing.T) {
+	var (
+		r          = require.New(t)
+		address    = common.BytesToAddress([]byte("CallStaticTestContract"))
+		statedb, _ = state.New(types.EmptyRootHash, state.NewDatabase(rawdb.NewMemoryDatabase()), nil)
+		config     = Config{}
+		vmctx      = newBlockContext()
+	)
+	statedb.CreateAccount(address)
+	// This example bytecode pushes 1 and 2 onto the stack, adds them, and returns the result. (Does NOT modify state)
+	statedb.SetCode(address, hexutil.MustDecode("0x600260010160005260206000F3"))
+	statedb.Finalise(true)
+	vmenv := NewEVM(vmctx, TxContext{}, statedb, params.AllEthashProtocolChanges, config)
+	ccVmenv := concreteEVM{
+		evm:    vmenv,
+		caller: common.Address{},
+	}
 
-	time := concreteBlockContext.Timestamp()
+	var (
+		gas            = uint64(100)
+		expectedReturn = []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x3}
+	)
 
-	r.Equal(time, expectedTime)
+	ret, remainingGas, err := ccVmenv.CallStatic(address, nil, gas)
+
+	r.NoError(err, "Call should not return an error")
+	r.NotNil(ret, "Return data should not be nil")
+	r.Less(remainingGas, gas, "Gas used should be less than the provided gas")
+
+	r.Equal(ret, expectedReturn, "Return data should be the result of the addition of 2 + 1")
+
 }
 
-func TestConcreteBlockNumber(t *testing.T) {
-	r := require.New(t)
-	concreteBlockContext := *newConcreteBlockContext()
-	expectedBlockNumber := uint64(0)
+func TestEVMCall(t *testing.T) {
+	var (
+		r          = require.New(t)
+		address    = common.BytesToAddress([]byte("CallTestContract"))
+		statedb, _ = state.New(types.EmptyRootHash, state.NewDatabase(rawdb.NewMemoryDatabase()), nil)
+		config     = Config{}
+		vmctx      = newBlockContext()
+	)
+	statedb.CreateAccount(address)
+	statedb.AddAddressToAccessList(address)
+	//This example bytecode saves value 1 in storage (Modify state)
+	statedb.SetCode(address, hexutil.MustDecode("0x6001600055600054602060005260206000f3"))
+	statedb.Finalise(true)
+	vmenv := NewEVM(vmctx, TxContext{}, statedb, params.AllEthashProtocolChanges, config)
+	ccVmenv := concreteEVM{
+		evm:    vmenv,
+		caller: common.Address{},
+	}
 
-	blockNumber := concreteBlockContext.BlockNumber()
+	var (
+		gas           = uint64(10000000)
+		expectedState = common.HexToHash("0x1")
+	)
 
-	r.Equal(expectedBlockNumber, blockNumber)
+	ret, remainingGas, err := ccVmenv.Call(address, nil, gas, new(uint256.Int))
+
+	r.NoError(err, "Call should not return an error")
+	r.NotNil(ret, "Return data should not be nil")
+	r.Less(remainingGas, gas, "Gas used should be less than the provided gas")
+
+	r.Equal(expectedState, statedb.GetState(address, common.Hash{}), "State should have changed by storing value 1 in Storage")
+
 }
 
-func TestConcreteGasLimit(t *testing.T) {
-	r := require.New(t)
-	concreteBlockContext := *newConcreteBlockContext()
-	expectedGasLimit := uint64(1000)
+func TestEVMCreate(t *testing.T) {
+	var (
+		r          = require.New(t)
+		statedb, _ = state.New(types.EmptyRootHash, state.NewDatabase(rawdb.NewMemoryDatabase()), nil)
+		config     = Config{}
+		vmctx      = newBlockContext()
+		gas        = uint64(10000)
+		code       = hexutil.MustDecode("0x600260010160005260206000F3")
+	)
 
-	gasLimit := concreteBlockContext.GasLimit()
+	statedb.Finalise(true)
+	vmenv := NewEVM(vmctx, TxContext{}, statedb, params.AllEthashProtocolChanges, config)
+	ccVmenv := concreteEVM{
+		evm:    vmenv,
+		caller: common.Address{},
+	}
 
-	r.Equal(gasLimit, expectedGasLimit)
-}
+	t.Run("Create", func(t *testing.T) {
+		expectedContractAddr := crypto.CreateAddress(ccVmenv.caller, statedb.GetNonce(ccVmenv.caller))
 
-func TestConcreteDifficulty(t *testing.T) {
-	r := require.New(t)
-	concreteBlockContext := *newConcreteBlockContext()
-	expectedDifficulty := uint256.MustFromBig(big.NewInt(23456146))
+		ret, addr, remainingGas, err := ccVmenv.Create(code, gas, new(uint256.Int))
 
-	difficulty := concreteBlockContext.Difficulty()
+		r.NoError(err, "Call should not return an error")
+		r.NotNil(ret, "Return data should not be nil")
+		r.Less(remainingGas, gas, "Gas used should be less than the provided gas")
+		r.Equal(expectedContractAddr, addr)
+		r.Equal(statedb.GetCode(addr), ret)
+	})
 
-	r.Equal(difficulty, expectedDifficulty)
-}
+	t.Run("Create2", func(t *testing.T) {
+		var (
+			salt                 = uint256.NewInt(3521)
+			codeAndHash          = &codeAndHash{code: code}
+			expectedContractAddr = crypto.CreateAddress2(ccVmenv.caller, salt.Bytes32(), codeAndHash.Hash().Bytes())
+		)
 
-func TestConcreteBaseFee(t *testing.T) {
-	r := require.New(t)
-	concreteBlockContext := *newConcreteBlockContext()
-	expectedBaseFee := uint256.MustFromBig(big.NewInt(739657255))
+		ret, addr, remainingGas, err := ccVmenv.Create2(code, gas, new(uint256.Int), uint256.NewInt(3521))
 
-	baseFee := concreteBlockContext.BaseFee()
-
-	r.Equal(baseFee, expectedBaseFee)
-}
-
-func TestConcreteCoinbase(t *testing.T) {
-	r := require.New(t)
-	concreteBlockContext := *newConcreteBlockContext()
-	expectedCoinbase := common.HexToAddress("0x0854167430392BBc2D15Dd1Cc17e761897AF31C9")
-
-	coinbase := concreteBlockContext.Coinbase()
-
-	r.Equal(coinbase, expectedCoinbase)
-}
-func TestConcreteRandom(t *testing.T) {
-	r := require.New(t)
-	concreteBlockContext := *newConcreteBlockContext()
-	expectedRandomHash := common.HexToHash("0xa91de29564820ffde10c59f8f429df5e29433da59643ad800bbe4e3959d936d7")
-
-	randomHash := concreteBlockContext.Random()
-
-	r.Equal(randomHash, expectedRandomHash)
+		r.NoError(err, "Call should not return an error")
+		r.NotNil(ret, "Return data should not be nil")
+		r.Less(remainingGas, gas, "Gas used should be less than the provided gas")
+		r.Equal(expectedContractAddr, addr)
+		r.Equal(statedb.GetCode(addr), ret)
+	})
 }
